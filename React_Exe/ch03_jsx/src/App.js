726cedc9ef06d327413f6f29cb2aa6a800677522
NCP08 <navercloudplatform08.gmail.com>
2024-05-17 13:21:02 +0900

    props 진행중...

--- React_Exe/ch03_jsx/src/App.js
+++ React_Exe/ch03_jsx/src/App.js
@@ -3,7 +3,10 @@ import "./App.css";
 // import Child from "./ex01_var/Child";
 // import Child from "./ex02_cond_op/Child";
 // import Child from "./ex03_and_op/Child";
-import Child from "./ex04_undefined/Child";
+// import Child from "./ex04_undefined/Child";
+// import Child from "./ex05_style/Child";
+// import Child from "./ex06_classname/Child";
+import Child from "./ex07_comment/Child";
 
 function App() {
   return <Child />;
