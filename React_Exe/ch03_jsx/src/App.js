f68d86c03e7c957c9be624212c0fd3314bf6b3a7
NCP08 <navercloudplatform08.gmail.com>
2024-05-17 11:57:43 +0900

    jsx문법 진행중...

---
+++ React_Exe/ch03_jsx/src/App.js
@@ -0,0 +1,12 @@
+import logo from "./logo.svg";
+import "./App.css";
+// import Child from "./ex01_var/Child";
+// import Child from "./ex02_cond_op/Child";
+// import Child from "./ex03_and_op/Child";
+import Child from "./ex04_undefined/Child";
+
+function App() {
+  return <Child />;
+}
+
+export default App;
