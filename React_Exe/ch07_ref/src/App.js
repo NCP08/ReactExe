19b846f570f272eefa8369618752c9dbf97504f1
NCP08 <navercloudplatform08.gmail.com>
2024-05-20 15:01:42 +0900

    엘리먼트 참조 ref

---
+++ React_Exe/ch07_ref/src/App.js
@@ -0,0 +1,11 @@
+import logo from "./logo.svg";
+import "./App.css";
+// import ValidationSample from "./ex02_validation/ValidationSample";
+// import ValidationSample from "./ex03_validation_ref/ValidationSample";
+import ValidationSample from "./ex04_validation_createRef/ValidationSample";
+
+function App() {
+  return <ValidationSample />;
+}
+
+export default App;
