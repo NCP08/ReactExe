12326edadb5e9bd7f55b5e1bdab26fec97f8bbd7
NCP08 <navercloudplatform08.gmail.com>
2024-05-20 15:31:41 +0900

    컴포넌트 ref

--- React_Exe/ch07_ref/src/App.js
+++ React_Exe/ch07_ref/src/App.js
@@ -2,10 +2,25 @@ import logo from "./logo.svg";
 import "./App.css";
 // import ValidationSample from "./ex02_validation/ValidationSample";
 // import ValidationSample from "./ex03_validation_ref/ValidationSample";
-import ValidationSample from "./ex04_validation_createRef/ValidationSample";
+// import ValidationSample from "./ex04_validation_createRef/ValidationSample";
+import { Component } from "react";
+import ScrollBox from "./ex05_component_ref/ScrollBox";
 
-function App() {
-  return <ValidationSample />;
+// function App() {
+//   return <ValidationSample />;
+// }
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <ScrollBox ref={(ref) => (this.ScrollBox = ref)} />
+        <button onClick={() => this.ScrollBox.scrollToBottom()}>
+          맨 밑으로
+        </button>
+      </div>
+    );
+  }
 }
 
 export default App;
