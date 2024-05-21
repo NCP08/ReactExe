12fdbcab9964776c79acb198c3cc073c2e1962db
NCP08 <navercloudplatform08.gmail.com>
2024-05-21 16:05:48 +0900

    props 사용

--- React_Exe/ch11_crud/src/App.js
+++ React_Exe/ch11_crud/src/App.js
@@ -1,7 +1,9 @@
 import logo from "./logo.svg";
 import "./App.css";
 // import SubApp from "./ex01_full_html/SubApp";
-import SubApp from "./ex02_divide_html/SubApp";
+// import SubApp from "./ex02_divide_html/SubApp";
+// import SubApp from "./ex03_divide_html/SubApp";
+import SubApp from "./ex04_props/SubApp";
 
 function App() {
   return <SubApp />;
