8617e8ac124a3fe127d9c51f286d889f39216583
NCP08 <navercloudplatform08.gmail.com>
2024-05-21 10:59:20 +0900

    Hook 실습

---
+++ React_Exe/ch10_hooks/src/App.js
@@ -0,0 +1,13 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+// import Counter from "./ex01_useState/Counter";
+// import Info from "./ex02_useState/Info";
+import Info from "./ex03_useEffect/Info";
+
+function App() {
+  // return <Counter />;
+  return <Info />;
+}
+
+export default App;
