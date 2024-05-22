cd312804e34bc6b54287061376061dc251df1b70
NCP08 <navercloudplatform08.gmail.com>
2024-05-22 11:44:46 +0900

    <Update/> 기능완료

--- React_Exe/ch11_crud/src/App.js
+++ React_Exe/ch11_crud/src/App.js
@@ -1,7 +1,7 @@
 import logo from "./logo.svg";
 import "./App.css";
 
-import SubApp from "./ex30_update/SubApp";
+import SubApp from "./ex31_update/SubApp";
 
 function App() {
   return <SubApp />;
