d5fc1aad2ae4c3443d4197a9897f2457fd199370
NCP08 <navercloudplatform08.gmail.com>
2024-05-22 11:39:01 +0900

    <Update/>에서 입력 수정 가능

--- React_Exe/ch11_crud/src/App.js
+++ React_Exe/ch11_crud/src/App.js
@@ -1,34 +1,7 @@
 import logo from "./logo.svg";
 import "./App.css";
-// import SubApp from "./ex01_full_html/SubApp";
-// import SubApp from "./ex02_divide_html/SubApp";
-// import SubApp from "./ex03_divide_html/SubApp";
-// import SubApp from "./ex04_props/SubApp";
-// import SubApp from "./ex05_list/SubApp";
-// import SubApp from "./ex06_nav_list/SubApp";
-// import SubApp from "./ex07_nav_list/SubApp";
-// import SubApp from "./ex08_nav_list/SubApp";
-// import SubApp from "./ex09_list_key/SubApp";
-// import SubApp from "./ex10_event/SubApp";
-// import SubApp from "./ex11_event_arrow/SubApp";
-// import SubApp from "./ex12_nav_event/SubApp";
-// import SubApp from "./ex13_state/SubApp";
-// import SubApp from "./ex14_state/SubApp";
-// import SubApp from "./ex15_state_id/SubApp";
-// import SubApp from "./ex16_create/SubApp";
-// import SubApp from "./ex17_create/SubApp";
-// import SubApp from "./ex18_create/SubApp";
-// import SubApp from "./ex19_create/SubApp";
-// import SubApp from "./ex20_create/SubApp";
-// import SubApp from "./ex21_create/SubApp";
-// import SubApp from "./ex22_create/SubApp";
-// import SubApp from "./ex23_create/SubApp";
-// import SubApp from "./ex24_create/SubApp";
-// import SubApp from "./ex25_update/SubApp";
-// import SubApp from "./ex26_update/SubApp";
-// import SubApp from "./ex27_update/SubApp";
-// import SubApp from "./ex28_update/SubApp";
-import SubApp from "./ex29_update/SubApp";
+
+import SubApp from "./ex30_update/SubApp";
 
 function App() {
   return <SubApp />;
