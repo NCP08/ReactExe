faa8d1b79c95bade0cf0c8ca7f0b9df02a9fb24d
NCP08 <navercloudplatform08.gmail.com>
2024-05-23 09:51:52 +0900

    SimpleButton styled

--- React_Exe/ch12_styled/src/App.js
+++ React_Exe/ch12_styled/src/App.js
@@ -1,13 +1,16 @@
 import logo from "./logo.svg";
 import "./App.css";
 import ReactButton from "./ex01_non_styled/ReactButton";
-import StyledButton from "./ex01_styled/StyledButton";
+import StyledButton from "./ex02_styled/StyledButton";
+// import SimpleButton from "./ex03_styled/SimpleButton";
+import SimpleButton from "./ex04_styled/SimpleButton";
 
 function App() {
   return (
     <>
       <ReactButton>안녕하세요</ReactButton>
       <StyledButton>감사합니다</StyledButton>
+      <SimpleButton>행복합니다</SimpleButton>
     </>
   );
 }
