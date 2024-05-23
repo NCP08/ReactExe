6cc582ac409dd538c2259a79602d04a65b9da011
NCP08 <navercloudplatform08.gmail.com>
2024-05-23 09:47:18 +0900

    Styled-button

--- React_Exe/ch12_styled/src/App.js
+++ React_Exe/ch12_styled/src/App.js
@@ -1,9 +1,15 @@
 import logo from "./logo.svg";
 import "./App.css";
 import ReactButton from "./ex01_non_styled/ReactButton";
+import StyledButton from "./ex01_styled/StyledButton";
 
 function App() {
-  return <ReactButton>안녕하세요</ReactButton>;
+  return (
+    <>
+      <ReactButton>안녕하세요</ReactButton>
+      <StyledButton>감사합니다</StyledButton>
+    </>
+  );
 }
 
 export default App;
