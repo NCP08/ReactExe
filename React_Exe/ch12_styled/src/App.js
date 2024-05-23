ddf4a55dd8123dbbf77c2ae9a484cc2c582d451e
NCP08 <navercloudplatform08.gmail.com>
2024-05-23 10:40:12 +0900

    Blocks styled-component

--- React_Exe/ch12_styled/src/App.js
+++ React_Exe/ch12_styled/src/App.js
@@ -9,18 +9,24 @@ import LargeButton from "./ex05_styled/LargeButton";
 // import PrimaryButton from "./ex07_styled/PrimaryButton";
 import PrimaryButton from "./ex08_styled/PrimaryButton";
 import SecondButton from "./ex09_styled/SecondButton";
+// import Blocks from "./ex10_styled/Blocks";
+import Blocks from "./ex11_styled/Blocks";
 
 function App() {
   return (
     <>
-      <ReactButton>안녕하세요</ReactButton>
+      {/* <ReactButton>안녕하세요</ReactButton>
       <StyledButton>감사합니다</StyledButton>
       <SimpleButton>행복합니다</SimpleButton>
       <LargeButton>존경합니다</LargeButton>
       <PrimaryButton>사랑합니다</PrimaryButton>
       <PrimaryButton primary>즐겁습니다</PrimaryButton>
       <SecondButton>Happy Happy</SecondButton>
-      <SecondButton primary>Congraturation!</SecondButton>
+      <SecondButton primary>Congraturation!</SecondButton> */}
+      <Blocks />
+      <Blocks column />
+      <Blocks stretch />
+      <Blocks baseline />
     </>
   );
 }
