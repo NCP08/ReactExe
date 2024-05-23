55e468f77e6a12e4b1c4c47fef5434af14c3b67c
NCP08 <navercloudplatform08.gmail.com>
2024-05-23 09:56:44 +0900

    LargeButton

--- React_Exe/ch12_styled/src/App.js
+++ React_Exe/ch12_styled/src/App.js
@@ -4,6 +4,7 @@ import ReactButton from "./ex01_non_styled/ReactButton";
 import StyledButton from "./ex02_styled/StyledButton";
 // import SimpleButton from "./ex03_styled/SimpleButton";
 import SimpleButton from "./ex04_styled/SimpleButton";
+import LargeButton from "./ex05_styled/LargeButton";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <ReactButton>안녕하세요</ReactButton>
       <StyledButton>감사합니다</StyledButton>
       <SimpleButton>행복합니다</SimpleButton>
+      <LargeButton>존경합니다</LargeButton>
     </>
   );
 }
