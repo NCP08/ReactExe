ad98ceee3e0bed018d7420f370de8bcfed2fb8f2
NCP08 <navercloudplatform08.gmail.com>
2024-05-23 10:10:35 +0900

    styled-component 확장

--- React_Exe/ch12_styled/src/App.js
+++ React_Exe/ch12_styled/src/App.js
@@ -5,6 +5,10 @@ import StyledButton from "./ex02_styled/StyledButton";
 // import SimpleButton from "./ex03_styled/SimpleButton";
 import SimpleButton from "./ex04_styled/SimpleButton";
 import LargeButton from "./ex05_styled/LargeButton";
+// import PrimaryButton from "./ex06_styled/PrimaryButton";
+// import PrimaryButton from "./ex07_styled/PrimaryButton";
+import PrimaryButton from "./ex08_styled/PrimaryButton";
+import SecondButton from "./ex09_styled/SecondButton";
 
 function App() {
   return (
@@ -13,6 +17,10 @@ function App() {
       <StyledButton>감사합니다</StyledButton>
       <SimpleButton>행복합니다</SimpleButton>
       <LargeButton>존경합니다</LargeButton>
+      <PrimaryButton>사랑합니다</PrimaryButton>
+      <PrimaryButton primary>즐겁습니다</PrimaryButton>
+      <SecondButton>Happy Happy</SecondButton>
+      <SecondButton primary>Congraturation!</SecondButton>
     </>
   );
 }
