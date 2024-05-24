201b592507402278ba2473315c1145ea22102207
NCP08 <navercloudplatform08.gmail.com>
2024-05-24 12:39:45 +0900

    동적으로 TodoListItem 추가

--- React_Exe/ch14_todoapp/src/App.js
+++ React_Exe/ch14_todoapp/src/App.js
@@ -1,13 +1,32 @@
 import "./App.css";
-import TodoTemplate from "./ex03_TodoList/TodoTemplate";
-import TodoInsert from "./ex03_TodoList/TodoInsert";
-import TodoList from "./ex03_TodoList/TodoList";
+import { useState } from "react";
+import TodoTemplate from "./ex04_TodoListItem/TodoTemplate";
+import TodoInsert from "./ex04_TodoListItem/TodoInsert";
+import TodoList from "./ex04_TodoListItem/TodoList";
 
 function App() {
+  const [todos, setTodos] = useState([
+    {
+      id: 1,
+      text: "리액트의 기초 알아보기",
+      checked: true,
+    },
+    {
+      id: 2,
+      text: "컴포넌트 스타일링 해보기",
+      checked: true,
+    },
+    {
+      id: 3,
+      text: "일정 관리 앱 만들어 보기",
+      checked: false,
+    },
+  ]);
+
   return (
     <TodoTemplate>
       <TodoInsert />
-      <TodoList />
+      <TodoList todos={todos} />
     </TodoTemplate>
   );
 }
