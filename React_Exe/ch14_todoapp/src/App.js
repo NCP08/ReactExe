a0b8b8288ebe6cff28946bebaccf8aef7fc62187
NCP08 <navercloudplatform08.gmail.com>
2024-05-24 12:27:04 +0900

    TodoApp의 UI완성

--- React_Exe/ch14_todoapp/src/App.js
+++ React_Exe/ch14_todoapp/src/App.js
@@ -1,11 +1,13 @@
 import "./App.css";
-import TodoTemplate from "./ex02_TodoInsert/TodoTemplate";
-import TodoInsert from "./ex02_TodoInsert/TodoInsert";
+import TodoTemplate from "./ex03_TodoList/TodoTemplate";
+import TodoInsert from "./ex03_TodoList/TodoInsert";
+import TodoList from "./ex03_TodoList/TodoList";
 
 function App() {
   return (
     <TodoTemplate>
       <TodoInsert />
+      <TodoList />
     </TodoTemplate>
   );
 }
