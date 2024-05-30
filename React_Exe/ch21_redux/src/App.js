225713be8eb58bbb8a21ed3d80d15ffb17fd1515
NCP08 <navercloudplatform08.gmail.com>
2024-05-30 14:47:53 +0900

    계층구조 파일 분할

--- React_Exe/ch21_redux/src/App.js
+++ React_Exe/ch21_redux/src/App.js
@@ -1,6 +1,6 @@
 import logo from "./logo.svg";
 import "./App.css";
-import MainApp from "./ex02_hierachy/MainApp";
+import MainApp from "./ex03_divide_files/MainApp";
 
 function App() {
   return <MainApp />;
