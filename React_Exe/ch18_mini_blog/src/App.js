8ff73f7e65e46c048e766235161cd0786b081f7e
NCP08 <navercloudplatform08.gmail.com>
2024-05-30 09:55:24 +0900

    MainPage 컴포넌트 완료

--- React_Exe/ch18_mini_blog/src/App.js
+++ React_Exe/ch18_mini_blog/src/App.js
@@ -1,6 +1,6 @@
 import logo from "./logo.svg";
 import "./App.css";
-import MiniBlogApp from "./ex03_mini_blog_app/MiniBlogApp";
+import MiniBlogApp from "./ex04_main_page/MiniBlogApp";
 
 function App() {
   return <MiniBlogApp />;
