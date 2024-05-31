88a251b0990ef2201a58c508068d441e0b279c1a
NCP08 <navercloudplatform08.gmail.com>
2024-05-31 11:42:54 +0900

    Owner엔티티와 Car엔티티의 1:N관계 형성

---
+++ CardDatabase/src/main/java/org/mind/carddatabase/domain/Owner.java
@@ -0,0 +1,25 @@
+package org.mind.carddatabase.domain;
+
+import lombok.*;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Entity
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class Owner {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long ownerid;
+
+    private String firstName;
+    private String lastName;
+
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "owner")
+    private List<Car> cars;
+}
