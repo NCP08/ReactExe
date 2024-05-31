cdf140b9270ef601ffe1642041ac741631e5e3d1
NCP08 <navercloudplatform08.gmail.com>
2024-05-31 10:33:58 +0900

    redux-toolkit 사용법 완료

--- React_Exe/ch22_redux_tk/src/App.js
+++ React_Exe/ch22_redux_tk/src/App.js
@@ -1,6 +1,6 @@
 import logo from "./logo.svg";
 import "./App.css";
-import MainApp from "./ex01_redux/MainApp";
+import MainApp from "./ex04_divide_files/MainApp";
 
 function App() {
   return <MainApp />;
