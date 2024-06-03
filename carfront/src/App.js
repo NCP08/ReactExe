8a449af5cf5b6dc5889aac98f2d6b81fc59b6fde
NCP08 <navercloudplatform08.gmail.com>
2024-06-03 16:05:33 +0900

    서버 주소를 외부 상수로 추출

--- carfront/src/App.js
+++ carfront/src/App.js
@@ -1,6 +1,6 @@
 import logo from "./logo.svg";
 import "./App.css";
-import MainApp from "./ex02_car_list/MainApp";
+import MainApp from "./ex03_server_url/MainApp";
 
 function App() {
   return <MainApp />;
