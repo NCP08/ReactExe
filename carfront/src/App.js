b8cca855e934ef0149d1ee26710d458c38373d07
NCP08 <navercloudplatform08.gmail.com>
2024-06-04 11:07:15 +0900

    AddCar의 ui기능 구현

--- carfront/src/App.js
+++ carfront/src/App.js
@@ -1,6 +1,6 @@
 import logo from "./logo.svg";
 import "./App.css";
-import MainApp from "./ex06_alram_message/MainApp";
+import MainApp from "./ex07_add_car/MainApp";
 
 function App() {
   return <MainApp />;
